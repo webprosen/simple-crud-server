712cd844ee21296e7b0ed1c86c0fa24978f21e09
Prosen Biswas <webprosen@gmail.com>
2023-05-07 21:56:45 +0600

    staging

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
 
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 // webprosen yDAqkLQKw90iw59e
 const uri = "mongodb+srv://webprosen:yDAqkLQKw90iw59e@cluster0.ww5yfzh.mongodb.net/?retryWrites=true&w=majority";
 
@@ -20,9 +22,53 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const userCollection = client.db('usersDB').collection('users');
+
+    app.get('/users', async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const user = await userCollection.findOne(query);
+      res.send(user);
+    });
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.put('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const user = req.body;
+      const filter = {_id: new ObjectId(id)};
+      const option = {upsert: true};
+      const updatedUser = {
+        $set: {
+          name: user.name,
+          email: user.email
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedUser, option);
+      res.send(result);
+    });
+
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log("You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -30,13 +76,11 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.use(cors());
-app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Server is running...');
+  res.send('Server is running...');
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
